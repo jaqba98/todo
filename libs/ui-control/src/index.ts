3debe69d3f2772f75a03694eaff4f087a189b16a
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-04-29 17:44:34 +0200

    feat: create ui-control project

---
+++ libs/ui-control/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/ui-control/ui-control.component';
---
+++ libs/ui-control/src/lib/ui-control/ui-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ui-control',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui-control.component.html',
+  styleUrl: './ui-control.component.css',
+})
+export class UiControlComponent {}
