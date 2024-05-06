0e579904a608e5a736a21c30853e8c27765120fd
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-06 20:39:41 +0200

    chore: copy the todo dist to the docs folder

--- libs/fe/src/lib/fe.config.ts
+++ libs/fe/src/lib/fe.config.ts
@@ -1,8 +1,10 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter } from "@angular/router";
+import { provideRouter, withHashLocation } from "@angular/router";
 
 import { pageRoutes } from "@todo/page";
 
 export const feConfig: ApplicationConfig = {
-  providers: [provideRouter(pageRoutes)]
+  providers: [
+    provideRouter(pageRoutes, withHashLocation())
+  ]
 };
