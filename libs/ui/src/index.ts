22895e78cac933dbec95c1598a7ba44e1e85fbb4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-06 19:00:11 +0200

    feat: create todo-list component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./lib/form/add-todo/add-todo.component";
+export * from "./lib/view/todo-list/todo-list.component";
---
+++ libs/ui/src/lib/view/todo-list/todo-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "lib-todo-list",
+  standalone: true,
+  templateUrl: "./todo-list.component.html",
+  styleUrl: "./todo-list.component.scss"
+})
+export class TodoListComponent {
+}
