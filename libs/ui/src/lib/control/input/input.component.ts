df884e950799222b29c6c97f87ee1362dd419a97
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-10 22:14:34 +0200

    refactor: input component

--- libs/ui/src/lib/control/input/input.component.ts
+++ libs/ui/src/lib/control/input/input.component.ts
@@ -1,17 +1,40 @@
-import { Component, Input } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component, Input, forwardRef } from "@angular/core";
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
+
 import { InputType } from "../../type/control.type";
 
 @Component({
   selector: "lib-input",
   standalone: true,
   imports: [FormsModule],
-  templateUrl: "./input.component.html"
+  templateUrl: "./input.component.html",
+  styleUrl: "./input.component.scss",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
   @Input() type: InputType = "text";
 
-  @Input() placeholder = "";
-
   @Input() value = "";
+
+  onChange: unknown = () => {};
+
+  onTouch: unknown = () => {};
+
+  writeValue(value: string) {
+    this.value = value;
+  }
+  
+  registerOnChange(fn: unknown): void {
+    this.onChange = fn;
+  }
+  
+  registerOnTouched(fn: unknown): void {
+    this.onTouch = fn;
+  }
 }
