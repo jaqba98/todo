2b61a1796b5f8f079e97fbf74effce8509b9e527
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-10 23:52:58 +0200

    refactor: ui project

--- libs/ui/src/lib/control/select/select.component.ts
+++ libs/ui/src/lib/control/select/select.component.ts
@@ -1,15 +1,46 @@
-import { Component, Input } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { Component, Input, forwardRef } from "@angular/core";
+import { FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
+
+import { OnChangeType, OnTouchType } from "../../type/accessor.type";
 
 @Component({
   selector: "lib-select",
   standalone: true,
-  imports: [CommonModule ,FormsModule],
-  templateUrl: "./select.component.html"
+  imports: [CommonModule, FormsModule],
+  templateUrl: "./select.component.html",
+  styleUrl: "./select.component.scss",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }
+  ]
 })
 export class SelectComponent {
   @Input() options: string[] = [];
 
   @Input() value = "";
+
+  onChange: OnChangeType<string> = (_value: string) => {};
+
+  onTouch: OnTouchType = () => {};
+
+  writeValue(value: string) {
+    this.value = value;
+  }
+  
+  registerOnChange(fn: OnChangeType<string>): void {
+    this.onChange = fn;
+  }
+  
+  registerOnTouched(fn: OnTouchType): void {
+    this.onTouch = fn;
+  }
+
+  onSelect(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    this.onChange(select.value);
+  }
 }
