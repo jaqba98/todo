d75f4c59b3abde5524af9f5b8756a6d41f0b49f5
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-10 23:58:33 +0200

    refactor: page project

--- libs/page/src/lib/tasks/tasks.component.ts
+++ libs/page/src/lib/tasks/tasks.component.ts
@@ -5,9 +5,8 @@ import { AddTodoFormComponent } from "@todo/ui";
 @Component({
   selector: "lib-tasks",
   standalone: true,
-  imports: [
-    AddTodoFormComponent
-  ],
-  templateUrl: "./tasks.component.html"
+  imports: [AddTodoFormComponent],
+  templateUrl: "./tasks.component.html",
+  styleUrl: "./tesk.component.scss"
 })
 export class TasksComponent {}
