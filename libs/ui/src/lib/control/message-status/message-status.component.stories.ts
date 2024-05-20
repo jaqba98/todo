934d28ca2848c85d2125b77fc88e3fb2653becdb
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-20 16:04:39 +0200

    feat: publish storybook to chromatic

--- libs/ui/src/lib/control/message-status/message-status.component.stories.ts
+++ libs/ui/src/lib/control/message-status/message-status.component.stories.ts
@@ -1,12 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/angular";
-import { MessageStatusComponent } from "./message-status.component";
 
-import { within } from "@storybook/testing-library";
-import { expect } from "@storybook/jest";
+import { MessageStatusComponent } from "./message-status.component";
 
 const meta: Meta<MessageStatusComponent> = {
   component: MessageStatusComponent,
-  title: "MessageStatusComponent",
+  title: "MessageStatusComponent"
 };
 export default meta;
 type Story = StoryObj<MessageStatusComponent>;
@@ -14,17 +12,6 @@ type Story = StoryObj<MessageStatusComponent>;
 export const Primary: Story = {
   args: {
     type: "info",
-    value: "",
-  },
-};
-
-export const Heading: Story = {
-  args: {
-    type: "info",
-    value: "",
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/message-status works!/gi)).toBeTruthy();
+    value: ""
   },
 };
--- libs/ui/src/lib/view/todo-list/todo-list.component.stories.ts
+++ libs/ui/src/lib/view/todo-list/todo-list.component.stories.ts
@@ -1,28 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/angular";
-import { TodoListComponent } from "./todo-list.component";
 
-import { within } from "@storybook/testing-library";
-import { expect } from "@storybook/jest";
+import { TodoListComponent } from "./todo-list.component";
 
 const meta: Meta<TodoListComponent> = {
   component: TodoListComponent,
-  title: "TodoListComponent",
+  title: "TodoListComponent"
 };
 export default meta;
 type Story = StoryObj<TodoListComponent>;
 
 export const Primary: Story = {
   args: {
-    title: "",
+    title: ""
   },
 };
 
-export const Heading: Story = {
-  args: {
-    title: "",
-  },
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    expect(canvas.getByText(/todo-list works!/gi)).toBeTruthy();
-  },
-};
