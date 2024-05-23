ab2e5c36d9e6e731bfe7ebae2df679a3419f8773
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-23 18:57:15 +0200

    feat: hide button add when meny is opened

--- libs/ui/src/lib/view/button-add-view/button-add-view.component.ts
+++ libs/ui/src/lib/view/button-add-view/button-add-view.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Subscription } from "rxjs";
 
 import { ButtonAddViewStoreService } from "@todo/store";
 
@@ -7,12 +9,26 @@ import { ButtonComponent } from "../../control/button/button.component";
 @Component({
   selector: "lib-button-add-view",
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [
+    CommonModule,
+    ButtonComponent
+  ],
   templateUrl: "./button-add-view.component.html",
   styleUrl: "./button-add-view.component.scss"
 })
-export class ButtonAddViewComponent {
+export class ButtonAddViewComponent implements OnDestroy {
+  isOpened!: boolean;
+
+  subscription: Subscription;
+
   constructor(private readonly store: ButtonAddViewStoreService) {
+    this.subscription = this.store.getModel().subscribe(model => {
+      this.isOpened = model.isOpened;
+    });
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onClick() {
