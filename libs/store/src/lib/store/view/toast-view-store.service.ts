171234172ce45565a4f380515000394508994eaf
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-29 15:20:18 +0200

    refactor: store project

--- libs/store/src/lib/store/view/toast-view-store.service.ts
+++ libs/store/src/lib/store/view/toast-view-store.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from "@angular/core";
 
-import { BaseStoreService } from "../base/base-store.service";
 import { ToastViewStoreModel } from "../../model/view/toast-view-store.model";
+import { BaseStoreService } from "../base/base-store.service";
 
 @Injectable({ providedIn: "root" })
 export class ToastViewStoreService extends BaseStoreService<ToastViewStoreModel> {
   constructor() {
-    super({
-      isVisible: false
-    });
+    super({ isVisible: false });
   }
 
-  switchIsVisible() {
-    this.model.isVisible = !this.model.isVisible;
-    this.setModel(this.model);
+  changeIsVisible(isVisible: boolean) {
+    this.model.isVisible = isVisible;
+    this.emitModel();
   }
 }
