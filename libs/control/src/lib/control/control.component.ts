ba93010bc23071eee12d663ceda9ad715a2138c3
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-05-30 17:52:45 +0200

    feat: create control project with storybook config

---
+++ libs/control/src/lib/control/control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-control',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+})
+export class ControlComponent {}
