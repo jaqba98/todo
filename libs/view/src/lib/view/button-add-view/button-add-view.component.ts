d0111d6616cfbd253b70fa03898323147d242e8d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-07 21:03:43 +0200

    fix: remove stories files from view

--- libs/view/src/lib/view/button-add-view/button-add-view.component.ts
+++ libs/view/src/lib/view/button-add-view/button-add-view.component.ts
@@ -1,31 +1,31 @@
-import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
+// import { Component } from "@angular/core";
+// import { CommonModule } from "@angular/common";
 
-@Component({
-  selector: "lib-button-add-view",
-  standalone: true,
-  imports: [
-    CommonModule
-  ],
-  templateUrl: "./button-add-view.component.html",
-  styleUrl: "./button-add-view.component.scss"
-})
-export class ButtonAddViewComponent {
-  // isOpened!: boolean;
+// @Component({
+//   selector: "lib-button-add-view",
+//   standalone: true,
+//   imports: [
+//     CommonModule
+//   ],
+//   templateUrl: "./button-add-view.component.html",
+//   styleUrl: "./button-add-view.component.scss"
+// })
+// export class ButtonAddViewComponent {
+//   // isOpened!: boolean;
 
-  // subscription: Subscription;
+//   // subscription: Subscription;
 
-  // constructor(private readonly store: ButtonAddViewStoreService) {
-  //   this.subscription = this.store.getModel().subscribe(model => {
-  //     this.isOpened = model.isOpened;
-  //   });
-  // }
+//   // constructor(private readonly store: ButtonAddViewStoreService) {
+//   //   this.subscription = this.store.getModel().subscribe(model => {
+//   //     this.isOpened = model.isOpened;
+//   //   });
+//   // }
   
-  // ngOnDestroy() {
-  //   this.subscription.unsubscribe();
-  // }
+//   // ngOnDestroy() {
+//   //   this.subscription.unsubscribe();
+//   // }
 
-  // onClick() {
-  //   this.store.switchIsOpened();
-  // }
-}
+//   // onClick() {
+//   //   this.store.switchIsOpened();
+//   // }
+// }
