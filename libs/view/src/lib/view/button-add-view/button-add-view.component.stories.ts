4044a23abc094908b8fa79e3d6464c7789ae9ed1
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-07 21:01:25 +0200

    feat: add simple view storybook

--- libs/view/src/lib/view/button-add-view/button-add-view.component.stories.ts
+++ libs/view/src/lib/view/button-add-view/button-add-view.component.stories.ts
@@ -1,15 +1,15 @@
-// import type { Meta, StoryObj } from "@storybook/angular";
+import type { Meta, StoryObj } from "@storybook/angular";
 
-// import { ButtonAddViewComponent } from "./button-add-view.component";
+import { ButtonAddViewComponent } from "./button-add-view.component";
 
-// const meta: Meta<ButtonAddViewComponent> = {
-//   component: ButtonAddViewComponent,
-//   title: "View/ButtonAddViewComponent",
-//   parameters: {
-//     layout: "fullscreen"
-//   }
-// };
-// export default meta;
-// type Story = StoryObj<ButtonAddViewComponent>;
+const meta: Meta<ButtonAddViewComponent> = {
+  component: ButtonAddViewComponent,
+  title: "View/ButtonAddViewComponent",
+  parameters: {
+    layout: "fullscreen"
+  }
+};
+export default meta;
+type Story = StoryObj<ButtonAddViewComponent>;
 
-// export const Primary: Story = {};
+export const Primary: Story = {};
--- libs/view/src/lib/view/button-add-view/button-add-view.component.ts
+++ libs/view/src/lib/view/button-add-view/button-add-view.component.ts
@@ -1,37 +1,31 @@
-// import { Component, OnDestroy } from "@angular/core";
-// import { CommonModule } from "@angular/common";
-// import { Subscription } from "rxjs";
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-// import { ButtonAddViewStoreService } from "@todo/store";
+@Component({
+  selector: "lib-button-add-view",
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: "./button-add-view.component.html",
+  styleUrl: "./button-add-view.component.scss"
+})
+export class ButtonAddViewComponent {
+  // isOpened!: boolean;
 
-// import { ButtonComponent } from "../../control/button/button.component";
+  // subscription: Subscription;
 
-// @Component({
-//   selector: "lib-button-add-view",
-//   standalone: true,
-//   imports: [
-//     CommonModule,
-//     ButtonComponent
-//   ],
-//   templateUrl: "./button-add-view.component.html",
-//   styleUrl: "./button-add-view.component.scss"
-// })
-// export class ButtonAddViewComponent implements OnDestroy {
-//   isOpened!: boolean;
-
-//   subscription: Subscription;
-
-//   constructor(private readonly store: ButtonAddViewStoreService) {
-//     this.subscription = this.store.getModel().subscribe(model => {
-//       this.isOpened = model.isOpened;
-//     });
-//   }
+  // constructor(private readonly store: ButtonAddViewStoreService) {
+  //   this.subscription = this.store.getModel().subscribe(model => {
+  //     this.isOpened = model.isOpened;
+  //   });
+  // }
   
-//   ngOnDestroy() {
-//     this.subscription.unsubscribe();
-//   }
+  // ngOnDestroy() {
+  //   this.subscription.unsubscribe();
+  // }
 
-//   onClick() {
-//     this.store.switchIsOpened();
-//   }
-// }
+  // onClick() {
+  //   this.store.switchIsOpened();
+  // }
+}
