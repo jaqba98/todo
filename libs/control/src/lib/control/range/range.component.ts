52eac04423f711cc6f00ffb028b190ce35e66bf8
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-10 20:01:40 +0200

    fix: select control logic

--- libs/control/src/lib/control/range/range.component.ts
+++ libs/control/src/lib/control/range/range.component.ts
@@ -1,21 +1,45 @@
-import { Component, Input } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import {
+  Component,
+  Input,
+  forwardRef
+} from "@angular/core";
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR
+} from "@angular/forms";
 
 import { LabelComponent } from "../label/label.component";
+import { OnChangeType, OnTouchType } from "../../type/accessor.type";
 
 @Component({
   selector: "lib-range",
   standalone: true,
   imports: [FormsModule, LabelComponent],
   templateUrl: "./range.component.html",
-  styleUrl: "./range.component.scss"
+  styleUrl: "./range.component.scss",
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RangeComponent),
+      multi: true
+    }
+  ]
 })
-export class RangeComponent {
+export class RangeComponent implements ControlValueAccessor {
   @Input() label: string;
 
   @Input() isError: boolean;
 
   @Input() value: number;
+
+  onChange: OnChangeType<string> = (value: string) => {
+    return value;
+  };
+
+  onTouch: OnTouchType = () => {
+    return;
+  };
 
   constructor() {
     this.label = "";
@@ -25,5 +49,17 @@ export class RangeComponent {
 
   getLabel() {
     return `${this.label}: ${this.value}%`;
+  }
+
+  writeValue(value: number) {
+    this.value = value;
+  }
+  
+  registerOnChange(fn: OnChangeType<string>): void {
+    this.onChange = fn;
+  }
+  
+  registerOnTouched(fn: OnTouchType): void {
+    this.onTouch = fn;
   }
 }
