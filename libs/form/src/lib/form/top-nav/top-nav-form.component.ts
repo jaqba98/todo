bb5d18f779ef95ce6c54391a3e46454d41052ba9
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-14 20:05:25 +0200

    feat: create basic hamburger menu

--- libs/form/src/lib/form/top-nav/top-nav-form.component.ts
+++ libs/form/src/lib/form/top-nav/top-nav-form.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { ButtonComponent } from "@todo/control";
 import { FlexViewComponent } from "@todo/view";
@@ -6,6 +7,7 @@ import { FlexViewComponent } from "@todo/view";
   selector: "lib-top-nav-form",
   standalone: true,
   imports: [
+    CommonModule,
     FlexViewComponent,
     ButtonComponent
   ],
@@ -13,4 +15,13 @@ import { FlexViewComponent } from "@todo/view";
   styleUrl: "./top-nav-form.component.scss"
 })
 export class TopNavFormComponent {
+  isOpened: boolean;
+
+  constructor() {
+    this.isOpened = false;
+  }
+  
+  onClick() {
+    this.isOpened = !this.isOpened;
+  }
 }
