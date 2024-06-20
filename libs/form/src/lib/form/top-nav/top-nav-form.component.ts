2f771fb1dc2404299513963c969dd82b247cbe00
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-06-20 18:53:45 +0200

    feat: create top nav component

--- libs/form/src/lib/form/top-nav/top-nav-form.component.ts
+++ libs/form/src/lib/form/top-nav/top-nav-form.component.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component, Renderer2 } from "@angular/core";
+import { Component, HostListener, Renderer2 } from "@angular/core";
 import { ButtonComponent } from "@todo/control";
-import { FlexViewComponent } from "@todo/view";
 
 @Component({
   selector: "lib-top-nav-form",
   standalone: true,
   imports: [
     CommonModule,
-    FlexViewComponent,
     ButtonComponent
   ],
   templateUrl: "./top-nav-form.component.html",
@@ -16,6 +14,12 @@ import { FlexViewComponent } from "@todo/view";
 })
 export class TopNavFormComponent {
   isOpened: boolean;
+
+  iconSrc: string;
+
+  iconAlt: string;
+
+  isMobile: boolean;
 
   options: string[] = [
     "Option 1",
@@ -26,17 +30,52 @@ export class TopNavFormComponent {
 
   constructor(private readonly renderer: Renderer2) {
     this.isOpened = false;
+    this.iconSrc = "assets/menu.svg";
+    this.iconAlt = "menu";
+    this.isMobile = this.getIsMobile();
+    this.isOpened = !this.isMobile;
   }
 
   onClick() {
     this.isOpened = !this.isOpened;
+    this.switchHamburgerIcon();
+  }
+
+  onClickOption() {
+    if (!this.getIsMobile()) return;
+    this.isOpened = false;
+    this.switchHamburgerIcon();
   }
 
   onBlur(): void {
     this.renderer.listen("document", "click", (click: Event) => {
+      if (!this.getIsMobile()) return;
       const el = click.target as HTMLElement;
       if (el.classList.contains("top-nav__control")) return;
       this.isOpened = false;
+      this.switchHamburgerIcon();
     });
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.isMobile = window.innerWidth < 834;
+    if (window.innerWidth > 834) {
+      this.isOpened = true;
+    }
+  }
+
+  private getIsMobile() {
+    return window.innerWidth < 834;
+  }
+
+  private switchHamburgerIcon() {
+    if (this.isOpened) {
+      this.iconSrc = "assets/close.svg";
+      this.iconAlt = "close";
+      return;
+    }
+    this.iconSrc = "assets/menu.svg";
+    this.iconAlt = "menu";
   }
 }
